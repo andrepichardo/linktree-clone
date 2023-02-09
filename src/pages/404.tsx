27b5e33e61370be3ea4f1985809286854b7f5dbb
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2023-02-09 12:56:45 -0400

    +pageTitle loading user

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,7 @@ const Error404 = () => {
   return (
     <>
       <Head>
-        <title>404 | LinkSpaceApp</title>
+        <title>Not Found | LinkSpaceApp</title>
         <meta name="description" content="A Linktree inspired app." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
--- src/pages/[creatorSlug]/index.tsx
+++ src/pages/[creatorSlug]/index.tsx
@@ -28,6 +28,7 @@ export default function Home() {
   >();
   const router = useRouter();
   const { creatorSlug } = router.query;
+  const PageTitle = (creatorSlug || 'loading...') + ' | LinkSpace';
 
   const onChange = (imageList: ImageListType) => {
     setImages(imageList);
@@ -180,7 +181,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>{creatorSlug} | LinkSpaceApp </title>
+        <title>{PageTitle}</title>
         <meta name="description" content="A Linktree inspired app." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
