73a524fc121712e295b29db7f9b43de59158ee93
andrepichardo <36863134+andrepichardo@users.noreply.github.com>
2023-02-13 20:59:06 -0400

    +fix toast rendering errors

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import toast from 'react-hot-toast';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    if (router.asPath == '/login') {
+      toast.remove();
+    }
+    if (router.asPath == '/signup') {
+      toast.remove();
+    }
+    if (router.asPath == '/') {
+      toast.remove();
+    }
+  }, [router]);
   return <Component {...pageProps} />;
 }
